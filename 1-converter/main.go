59834de25d299bcfbcf207bfb51a07e415adefdd
Vladislav Rychkov <vvrych3@gmail.com>
2024-08-29 09:37:59 +0300

    second hm

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -3,11 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	var rub float64 = 1000
+	rub := getUserData()
 	const USDinEUR = 0.9
 	const USDinRUB = 91.5
 	const EURinRUB = USDinRUB / USDinEUR
 
-	fmt.Println(rub/USDinRUB)
+	fmt.Println("В долларах рубли",rub/USDinRUB)
 	fmt.Println(rub/EURinRUB)
+}
+
+func getUserData() float64 {
+	var userData float64
+	fmt.Println("Введите сумму")
+	fmt.Scan(&userData)
+	return userData
+}
+
+func convertCurrency(sum float64, base string, target string) float64 {
+
 }
