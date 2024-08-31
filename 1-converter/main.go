310ef147db6b2616484b7fb8b1847bbe4894b2ca
Vladislav Rychkov <vvrych3@gmail.com>
2024-08-31 21:44:34 +0300

    make third homework add input and convert

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -2,23 +2,79 @@ package main
 
 import "fmt"
 
-func main() {
-	rub := getUserData()
-	const USDinEUR = 0.9
-	const USDinRUB = 91.5
-	const EURinRUB = USDinRUB / USDinEUR
+const USDinEUR = 0.9
+const USDinRUB = 91.5
 
-	fmt.Println("В долларах рубли",rub/USDinRUB)
-	fmt.Println(rub/EURinRUB)
+func main() {
+	base, sum, target := getUserData()
+	res := convertCurrency(sum, base, target)
+	fmt.Printf("%f %s в %s: %f ", sum, base, target, res)
 }
 
-func getUserData() float64 {
-	var userData float64
-	fmt.Println("Введите сумму")
-	fmt.Scan(&userData)
-	return userData
+func getUserData() (string, float64, string) {
+	var baseCurrency string
+	var sumValue float64
+	var targetCurrenct string
+	for {
+		fmt.Println("Введите исходную валюту: RUB | USD | EUR")
+		fmt.Scan(&baseCurrency)
+		if baseCurrency == "RUB" || baseCurrency == "USD" || baseCurrency == "EUR" {
+			break
+		} else {
+			fmt.Println("Вы ввели неправильно валюту, попробуйте ещё раз")
+		}
+	}
+	for {
+		fmt.Println("Введите сумму:")
+		fmt.Scan(&sumValue)
+		if sumValue >= 0 {
+			break
+		} else {
+			fmt.Println("Вы ввели неправильно сумму, попробуйте ещё раз")
+		}
+	}
+	for {
+		fmt.Println("Введите целевую валюту(отличную исходной): RUB | USD | EUR")
+		fmt.Scan(&targetCurrenct)
+		
+		if baseCurrency == targetCurrenct {
+			fmt.Println("Целевая валюта идентична исходной, выберите другую")
+			continue
+		}
+		if targetCurrenct == "RUB" || targetCurrenct == "USD" || targetCurrenct == "EUR" {
+			break
+		} else {
+			fmt.Println("Вы ввели неправильно валюту, попробуйте ещё раз")
+		}
+	}
+	return baseCurrency, sumValue, targetCurrenct
 }
 
 func convertCurrency(sum float64, base string, target string) float64 {
-
+	var res float64
+	if base == "USD" {
+		if target == "EUR" {
+			res = sum * USDinEUR
+		}
+		if target == "RUB" {
+			res = sum * USDinRUB
+		}
+	}
+	if base == "EUR" {
+		if target == "USD" {
+			res = sum / USDinEUR
+		}
+		if target == "RUB" {
+			res = sum / USDinEUR * USDinRUB
+		}
+	}
+	if base == "RUB" {
+		if target == "USD" {
+			res = sum / USDinRUB
+		}
+		if target == "EUR" {
+			res = sum / USDinRUB * USDinEUR
+		}
+	}
+	return res
 }
